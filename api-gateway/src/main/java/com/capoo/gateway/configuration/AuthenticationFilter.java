d6af5581a03ad8a91186e114096e0c4e3aa65c58
tuancapoo <tuan.nguyencapoo@hcmut.edu.vn>
2026-03-19 22:57:59 +0700

    Api gateway Authentication Init

--- api-gateway/src/main/java/com/capoo/gateway/configuration/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/capoo/gateway/configuration/AuthenticationFilter.java
@@ -7,18 +7,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
 import java.util.List;
 @Component
 @Slf4j
@@ -27,9 +32,23 @@ import java.util.List;
 public class AuthenticationFilter implements GlobalFilter, Ordered {
     IdentityService identityService;
     ObjectMapper objectMapper;
+    @NonFinal
+    String[] publicEndpoints=new String[]{
+            "/identity/auth/.*"
+    };
+    @Value("${app.api-prefix}")
+    @NonFinal
+    String apiPrefix;
+
+    private static final String[] PUBLIC_ENDPOINTS = {
+    };
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("AuthenticationFilter");
+        //public endpoints
+        if (isPublicEndpoint(exchange.getRequest())) {
+            return chain.filter(exchange);
+        }
         // Get Token from header
         List<String> authHeader=exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION);
         if (CollectionUtils.isEmpty(authHeader)) {
@@ -49,13 +68,17 @@ public class AuthenticationFilter implements GlobalFilter, Ordered {
     public int getOrder() {
         return -1;
     }
+    private boolean isPublicEndpoint(ServerHttpRequest request) {
+        return Arrays.stream(publicEndpoints)
+                .anyMatch(s->request.getURI().getPath().matches(apiPrefix+s));
+    }
 
     Mono<Void> unAuthenticatedResponse(ServerHttpResponse response) {
         ApiResponse<?> apiResponse=ApiResponse.builder()
                 .code(HttpStatus.UNAUTHORIZED.value())
                 .message("Unauthenticated")
                 .build();
-        String body=null;
+        String body;
         try {
             body=objectMapper.writeValueAsString(apiResponse);
         } catch (JsonProcessingException e) {
